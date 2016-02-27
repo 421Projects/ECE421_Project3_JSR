af34fd9ad97e8594b28b24b92ee02cd3c00fe000
suhussai <suhussai@ualberta.ca>
2016-02-27 16:58:30 -0700

    - add better precondition checking for block
    - checks if block is required
    - when a block is provided, checks if it conforms to the kind of block needed for sorting (ie checks for spaceship implementation (essentially))

--- src/assignment3_main.rb
+++ src/assignment3_main.rb
@@ -13,33 +13,68 @@ class Array
     invariant(self) {self.hash == @original_array_hash}
     invariant(@duration) {@duration.hash == @original_duration_hash}
 
-    def precondition_block_check(&block)
-        raise ArgumentError, "Block doesn't accept two arguments." unless
-            block.arity == 2
-        b = Proc.new do
-            #|x,y| x<=>y
-            |x,y|
-            block.call(x,y)
+    def precondition_block_check(block)
+        tmp_array = self.clone
+
+        if tmp_array.uniq.empty?
+            return
+        elsif tmp_array.uniq.size >= 2
+            tmp_array.uniq!
+            first = tmp_array[0]
+            sec = tmp_array[1]
+            raise ArgumentError, "Block doesn't accept two arguments." unless
+                block.arity == 2
+
+            # ensure that the return values are acceptable
+            acceptable_return_values = [-1,0,1]
+            raise ArgumentError, "Block return value incorrect." unless
+                acceptable_return_values.member?(block.call(first,sec))
+            raise ArgumentError, "Block return value incorrect." unless
+                acceptable_return_values.member?(block.call(sec,first))
+            raise ArgumentError, "Block return value incorrect." unless
+                acceptable_return_values.member?(block.call(first,first))
+            raise ArgumentError, "Block return value incorrect." unless
+                acceptable_return_values.member?(block.call(sec,sec))
+            raise ArgumentError, "Block return value incorrect." unless
+                (block.call(sec,sec) == block.call(first,first))
+
+            # ensure that each of the three different cases
+            # returns a different value
+            raise ArgumentError, "Block return value incorrect." unless
+                [block.call(first,sec), block.call(sec,first), block.call(first,first)].uniq.size == 3
         end
+    end
 
-        # ensure that the return values are acceptable
-        acceptable_return_values = [-1,0,1]
-        raise ArgumentError, "Block return value incorrect." unless
-            acceptable_return_values.member?(b.call(1,1))
-        raise ArgumentError, "Block return value incorrect." unless
-            acceptable_return_values.member?(b.call(2,1))
-        raise ArgumentError, "Block return value incorrect." unless
-            acceptable_return_values.member?(b.call(1,2))
+    def block_required?
+        tmp_array = self.clone
+        if tmp_array.empty? or tmp_array.uniq == nil or tmp_array.uniq.size == 1
+            return false
+        elsif tmp_array.uniq.size >= 2
+            tmp_array.uniq!
+            first = tmp_array[0]
+            sec = tmp_array[1]
 
-        # ensure that each of the three different cases
-        # returns a different value
-        raise ArgumentError, "Block return value incorrect." unless
-            [b.call(1,1), b.call(2,1), b.call(1,2)].uniq.size == 3
+            # ensure that the return values are acceptable
+            acceptable_return_values = [-1,0,1]
+
+            if acceptable_return_values.member?(first <=> sec) and
+              acceptable_return_values.member?((sec <=> first)) and
+              acceptable_return_values.member?((first <=> first)) and
+              acceptable_return_values.member?((sec <=> sec)) and
+              ((sec <=> sec) == (first <=> first)) and
+              [first <=> sec, sec <=> first, first <=> first].uniq.size == 3
+                return false
+            else
+                return true
+            end
+        end
     end
 
     Contract Contracts::Pos, Maybe[Proc] => Array
     def multithreaded_sort(duration, &block)
-        precondition_block_check(&block) if block != nil
+        raise ArgumentError, "Block required." if (block_required?) and (block_given? == false)
+        precondition_block_check(block) if block_given?
+
         @duration = duration
         @original_array_hash = self.hash
         @original_duration_hash = @duration.hash
