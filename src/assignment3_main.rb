1fa3cdd94f7a05342020d097b89b856b200f7a18
suhussai <suhussai@ualberta.ca>
2016-02-26 20:54:23 -0700

    add invariant checking for duration as well

--- src/assignment3_main.rb
+++ src/assignment3_main.rb
@@ -4,15 +4,26 @@ class Array
     include Contracts::Core
     include Contracts::Builtin
     include Contracts::Invariants
+
+    # invariant stuff
     @original_array_hash = 0
+    @original_duration_hash = 0
+    @duration = 0
 
     invariant(self) {self.hash == @original_array_hash}
+    invariant(@duration) {@duration.hash == @original_duration_hash}
 
     Contract Num => Array
     def multithreaded_sort(duration)
+        @duration = duration
         @original_array_hash = self.hash
+        @original_duration_hash = @duration.hash
+
+        # use @duration instead of duration from this point on
+        # and don't change the above lines
 
         #self[0] = 10
+        @duration = 1
 
         return []
     end
